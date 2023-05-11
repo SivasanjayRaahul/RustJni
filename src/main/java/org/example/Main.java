3aaf1d05e855183ab4ffbf2576932de8bfb3f197
SivasanjayRaahul <sivasanjay.mohan@thoughtworks.com>
2023-05-11 16:11:45 +0530

    [Sivasanjay] Add rust lib to print name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
 public class Main {
+
+    private static native String hello(String input);
+
+    static {
+        // This actually loads the shared object that we'll be creating.
+        // The actual location of the .so or .dll may differ based on your
+        // platform.
+        System.load("/Users/sivasanjayraahulmohan/Documents/E4R/RustJni/jni/src/main/rust/rust_lib/target/release/librust_lib.dylib");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String output = Main.hello("Sivasanjay");
+        System.out.println(output);
     }
 }
